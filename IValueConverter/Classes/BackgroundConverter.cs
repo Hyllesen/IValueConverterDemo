644b9bb29deff01c2e1396c7c76d2979828f0eea
stefan <stefan@DESKTOP-QF299TO>
2016-09-11 01:20:46 +0200

    Add project files.

---
+++ IValueConverter/Classes/BackgroundConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace IValueConverterProject.Classes
+{
+    public class BackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null)
+            {
+                if (value.ToString() == "Male")
+                {
+                    Console.WriteLine("Male");
+                    return new SolidColorBrush(new Color()
+                    {
+                        A = 255,
+                        R = 255,
+                        G = 255,
+                        B = 0
+                    });
+                }
+                else
+                {
+                    Console.WriteLine("FeMale");
+                    return new SolidColorBrush(new Color()
+                    {
+                        A = 255,
+                        R = 0,
+                        G = 255,
+                        B = 255
+                    });
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IValueConverter/Classes/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IValueConverterProject.Classes
+{
+    public class Person : INotifyPropertyChanged
+    {
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set {
+                onPropertyChanged("Name");
+                name = value;
+            }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set {
+                age = value;
+                onPropertyChanged("Age");
+            }
+        }
+
+        private string gender;
+
+        public string Gender
+        {
+            get { return gender; }
+            set {
+                gender = value;
+                onPropertyChanged("Gender");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
